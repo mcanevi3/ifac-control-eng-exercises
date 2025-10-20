265bb77e336b990f6bfdcf55a0aab5305b63016c
Mehmet Canevi <m.canevi3@gmail.com>
2025-10-20 16:38:54 +0300

    small fix

--- test_pid/run.sh
+++ test_pid/run.sh
@@ -11,7 +11,8 @@ do
 			text2=$text2"\def\xone{"$y"}";
 			
 			echo -e $text1"\n"$text2 > $file
-			eval "pdflatex -output-directory='Files' -jobname=$x$y main.tex "
+			eval "pdflatex -output-directory='Files' \
+			-jobname=$x$y main.tex "
 			eval "rm Files/*.log"
 			eval "rm Files/*.aux"
 			eval "rm digits.tex"
