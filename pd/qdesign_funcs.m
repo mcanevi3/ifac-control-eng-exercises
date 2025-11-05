a1a1b222faea6efada111d5e95e701015a24ada4
Mehmet Canevi <m.canevi3@gmail.com>
2025-11-05 18:06:06 +0300

    small fix

--- pd/qdesign_funcs.m
+++ pd/qdesign_funcs.m
@@ -14,7 +14,7 @@ ds=collect(expand(ds),s);
 
 syms k z real;
 pcs=ds+ns*k*(s+z);
-pcs=collect(expand(pcs),s);
+pcs=collect(expand(pcs),s); 
 
 sigma=(p0+p1)/2;
 tsdes=2/sigma;
@@ -49,3 +49,24 @@ fun_angleFalse2=matlabFunction(180-angleZero+angleOne+anglezVal);
 fun_angleFalse3=matlabFunction(angleZero+180-angleOne+anglezVal);
 fun_angleFalse4=matlabFunction(angleZero+angleOne+anglezVal);
 
+% %% 
+% syms p0 p1 k z real
+% ns=1;
+% ds=(s+p1)*(s+p0);
+% ds=collect(expand(ds),s);
+
+% syms k z real;
+% pcs=ds+ns*k*(s+z);
+% pcs=collect(expand(pcs),s);
+
+% sigma=(p0+p1)/2;
+% tsdes=2/sigma;
+% sigmades=4/tsdes;
+
+% wn=sigmades/zeta;
+% wdes=sqrt(1-zeta^2)*wn;
+% pds=s^2+2*zeta*wn*s+wn^2;
+
+% prob=coeffs(pcs,s,'all')==coeffs(pds,s,'all');
+
+% sol=solve(prob,[k,z]);
