1880d96cc9d134158115549ee26ce6ed941fc684
Mehmet Canevi <m.canevi3@gmail.com>
2025-11-10 14:22:07 +0300

    small fix about paper

--- test_pdf/paper_img.sh
+++ test_pdf/paper_img.sh
@@ -14,6 +14,30 @@ do
 			eval "pdflatex -output-directory='paper_img' -jobname=q2_2 paperq2_2.tex "
 			eval "pdflatex -output-directory='paper_img' -jobname=q2_2 paperq2_2.tex "
 
+			echo "\def\xzero{"$x"}\def\xone{"$y"}" > $file
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_q paperq3_q.tex "
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_q paperq3_q.tex "
+
+			echo "\def\xzero{"$x"}\def\xone{"$y"}" > $file
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_correct paperq3_correct.tex "
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_correct paperq3_correct.tex "
+
+			echo "\def\xzero{"$x"}\def\xone{"$y"}" > $file
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f1 paperq3_f1.tex "
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f1 paperq3_f1.tex "
+
+			echo "\def\xzero{"$x"}\def\xone{"$y"}" > $file
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f2 paperq3_f2.tex "
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f2 paperq3_f2.tex "
+
+			echo "\def\xzero{"$x"}\def\xone{"$y"}" > $file
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f3 paperq3_f3.tex "
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f3 paperq3_f3.tex "
+
+			echo "\def\xzero{"$x"}\def\xone{"$y"}" > $file
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f4 paperq3_f4.tex "
+			eval "pdflatex -output-directory='paper_img' -jobname=q3_f4 paperq3_f4.tex "
+
 			eval "rm paper_img/*.log"
 			eval "rm paper_img/*.aux"
 			eval "rm digits.tex"
