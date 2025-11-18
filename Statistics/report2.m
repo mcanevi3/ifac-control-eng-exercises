dc74e069052ab42f8db3c5eb1445755fa1438a54
Mehmet Canevi <m.canevi3@gmail.com>
2025-11-18 08:18:06 +0300

    statistics average added

--- Statistics/report2.m
+++ Statistics/report2.m
@@ -132,24 +132,29 @@ post_tr=post_tr(1:end-2);
 mu2025(4)=mean(post_tr);sigma2025(4)=std(post_tr);
 
 %% plot gaussian distribution
-exam_type=["Final","Pre Test","Post Test"];
+exam_type=["Final","Pre Test","Post Test","Average"];
 
 figure(1);clf;gcf1=gcf;
 
 ii=1;
-subplot(3,1,ii);cla;hold on;grid on;ax1=gca;xlabel(ax1,'Grade');
+subplot(4,1,ii);cla;hold on;grid on;ax1=gca;xlabel(ax1,'Grade');
 ylabel(ax1,'Probability Density');legend(ax1,"show");
 title(ax1,exam_type(ii)+":Gaussian Distribution of Grades");
 
 ii=2;
-subplot(3,1,ii);cla;hold on;grid on;ax2=gca;xlabel(ax2,'Grade');
+subplot(4,1,ii);cla;hold on;grid on;ax2=gca;xlabel(ax2,'Grade');
 ylabel(ax2,'Probability Density');legend(ax2,"show");
 title(ax2,exam_type(ii)+":Gaussian Distribution of Grades");
 
 ii=3;
-subplot(3,1,ii);cla;hold on;grid on;ax3=gca;xlabel(ax3,'Grade');
+subplot(4,1,ii);cla;hold on;grid on;ax3=gca;xlabel(ax3,'Grade');
 ylabel(ax3,'Probability Density');legend(ax3,"show");
 title(ax3,exam_type(ii)+":Gaussian Distribution of Grades");
+
+ii=4;
+subplot(4,1,ii);cla;hold on;grid on;ax4=gca;xlabel(ax4,'Grade');
+ylabel(ax4,'Probability Density');legend(ax4,"show");
+title(ax4,exam_type(ii)+":Gaussian Distribution of Grades");
 
 grades_vec = 0:0.01:100;
 y=@(mu,sigma)1/(sigma*sqrt(2*pi)) * exp(-(grades_vec - mu).^2 / (2*sigma^2));
@@ -181,45 +186,64 @@ plot(ax3,grades_vec,y(mu2023(ii),sigma2023(ii)),'g','LineWidth',2,'DisplayName',
 plot(ax3,grades_vec,y(mu2024(ii),sigma2024(ii)),'g','LineWidth',2,'DisplayName','2024');
 plot(ax3,grades_vec,y(mu2025(ii),sigma2025(ii)),'g','LineWidth',2,'DisplayName','2025');
 
+ii=4;
+plot(ax4,grades_vec,y(mu2019(ii),sigma2019(ii)),'g','LineWidth',2,'DisplayName','2019');
+plot(ax4,grades_vec,y(mu2020(ii),sigma2020(ii)),'r','LineWidth',2,'DisplayName','2020');
+plot(ax4,grades_vec,y(mu2021(ii),sigma2021(ii)),'r','LineWidth',2,'DisplayName','2021');
+plot(ax4,grades_vec,y(mu2022(ii),sigma2022(ii)),'g','LineWidth',2,'DisplayName','2022');
+plot(ax4,grades_vec,y(mu2023(ii),sigma2023(ii)),'g','LineWidth',2,'DisplayName','2023');
+plot(ax4,grades_vec,y(mu2024(ii),sigma2024(ii)),'g','LineWidth',2,'DisplayName','2024');
+plot(ax4,grades_vec,y(mu2025(ii),sigma2025(ii)),'g','LineWidth',2,'DisplayName','2025');
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(2);clf;gcf2=gcf;
 
 ii=1;
-subplot(3,1,ii);cla;hold on;grid on;ax1=gca;
+subplot(4,1,ii);cla;hold on;grid on;ax1=gca;
 plot(ax1,2019:2025,[mu2019(ii),mu2020(ii),mu2021(ii),mu2022(ii),mu2023(ii),mu2024(ii),mu2025(ii)],'LineWidth',2)
 title(ax1,exam_type(ii));
 ii=2;
-subplot(3,1,ii);cla;hold on;grid on;ax2=gca;
+subplot(4,1,ii);cla;hold on;grid on;ax2=gca;
 plot(ax2,2019:2025,[mu2019(ii),mu2020(ii),mu2021(ii),mu2022(ii),mu2023(ii),mu2024(ii),mu2025(ii)],'LineWidth',2)
 title(ax2,exam_type(ii));
 ii=3;
-subplot(3,1,ii);cla;hold on;grid on;ax3=gca;
+subplot(4,1,ii);cla;hold on;grid on;ax3=gca;
 plot(ax3,2019:2025,[mu2019(ii),mu2020(ii),mu2021(ii),mu2022(ii),mu2023(ii),mu2024(ii),mu2025(ii)],'LineWidth',2)
 title(ax3,exam_type(ii));
+ii=4;
+subplot(4,1,ii);cla;hold on;grid on;ax4=gca;
+plot(ax4,2019:2025,[mu2019(ii),mu2020(ii),mu2021(ii),mu2022(ii),mu2023(ii),mu2024(ii),mu2025(ii)],'LineWidth',2)
+title(ax4,exam_type(ii));
 
 util("plot_area",ax1,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
 util("plot_area",ax2,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
 util("plot_area",ax3,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
+util("plot_area",ax4,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(3);clf;gcf3=gcf;
 
 ii=1;
-subplot(3,1,ii);cla;hold on;grid on;ax1=gca;
+subplot(4,1,ii);cla;hold on;grid on;ax1=gca;
 plot(ax1,2019:2025,[sigma2019(ii),sigma2020(ii),sigma2021(ii),sigma2022(ii),sigma2023(ii),sigma2024(ii),sigma2025(ii)],'LineWidth',2)
 title(ax1,exam_type(ii));
 ii=2;
-subplot(3,1,ii);cla;hold on;grid on;ax2=gca;
+subplot(4,1,ii);cla;hold on;grid on;ax2=gca;
 plot(ax2,2019:2025,[sigma2019(ii),sigma2020(ii),sigma2021(ii),sigma2022(ii),sigma2023(ii),sigma2024(ii),sigma2025(ii)],'LineWidth',2)
 title(ax2,exam_type(ii));
 ii=3;
-subplot(3,1,ii);cla;hold on;grid on;ax3=gca;
+subplot(4,1,ii);cla;hold on;grid on;ax3=gca;
 plot(ax3,2019:2025,[sigma2019(ii),sigma2020(ii),sigma2021(ii),sigma2022(ii),sigma2023(ii),sigma2024(ii),sigma2025(ii)],'LineWidth',2)
 title(ax3,exam_type(ii));
+ii=4;
+subplot(4,1,ii);cla;hold on;grid on;ax4=gca;
+plot(ax4,2019:2025,[sigma2019(ii),sigma2020(ii),sigma2021(ii),sigma2022(ii),sigma2023(ii),sigma2024(ii),sigma2025(ii)],'LineWidth',2)
+title(ax4,exam_type(ii));
 
 util("plot_area",ax1,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
 util("plot_area",ax2,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
 util("plot_area",ax3,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
+util("plot_area",ax4,[2020-0.2,0],[2021+0.2,100],[1,0,0],0.1)
 
 exportgraphics(gcf1, "img/gauss1.pdf", ...
 ContentType="vector", ...
